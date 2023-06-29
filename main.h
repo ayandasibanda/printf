e3d23ea9b4b3ba8734ce2982e6fa32fd6682e6c6
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-06-29 16:43:12 -0700

    the printf project

--- main.h
+++ main.h
@@ -1,63 +1,117 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
 
 /**
- * _printf - Prints formatted output to the standard output.
- * @format: The format string specifying the output format.
- * @...: Additional arguments to be formatted according to the format string.
+ * struct fmt - Struct op
  *
- * Return: The number of characters printed.
+ * @fmt: The format.
+ * @fn: The function associated.
  */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
+
 int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/**
- * _putchar - Writes a character to the standard output.
- * @c: The character to be written.
- *
- * Return: On success, the character written. On error, -1 is returned.
- */
-int _putchar(char c);
+/****************** FUNCTIONS ******************/
 
-/**
- * print_number - Prints an integer to the standard output.
- * @number: The integer to be printed.
- *
- * Return: The number of characters printed.
- */
-int print_number(int number);
+/* Funtions to print chars and strings */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
-/**
- * print_string - Prints a string to the standard output.
- * @s: The string to be printed.
- *
- * Return: The number of characters printed.
- */
-int print_string(char *s);
+/* Functions to print numbers */
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
-/**
- * print_char - Prints a single character to the standard output.
- * @c: The character to be printed.
- *
- * Return: The number of characters printed.
- */
-int print_char(char c);
+int print_hexa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/**
- * print_pointer - Prints a pointer address to the standard output.
- * @ptr: The pointer to be printed.
- *
- * Return: The number of characters printed.
- */
-int print_pointer(void *ptr);
+/* Function to print non printable characters */
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
-/**
- * print_hex - Prints a hexadecimal number to the standard output.
- * @number: The number to be printed.
- *
- * Return: The number of characters printed.
- */
-int print_hex(unsigned long int number);
+/* Funcion to print memory address */
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
-#endif
+/* Funciotns to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
+/*Function to print string in reverse*/
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/*Function to print a string in rot 13*/
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/* width handler */
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind,
+char buffer[],
+	int flags, int width, int precision, int size);
+
+/****************** UTILITY ******************/
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+
+#endif /* MAIN_H */
--- print_csp.c
+++ print_csp.c
@@ -8,10 +8,9 @@
  *
  * Return: The number of characters printed
  */
-int print_char(char c)
+static int print_char(char c)
 {
-	_putchar(c);
-	return (1);
+        return putchar(c);
 }
 
 /**
@@ -20,65 +19,15 @@ int print_char(char c)
  *
  * Return: The number of characters printed
  */
-int print_string(char *s)
+static int print_string(char *s)
 {
-	int count = 0;
+        int count = 0;
 
-	while (*s != '\0')
-	{
-		putchar(*s);
-		s++;
-		count++;
-	}
+        while (*s != '\0')
+        {
+                count += putchar(*s);
+                s++;
+        }
 
-	return (count);
-}
-
-/**
- * _printf - Prints formatted output according to a format
- * @format: A character string composed of zero or more directives
- *
- * Return: The number of characters printed excluding the null byte
- */
-int _printf(const char *format, ...)
-{
-	int count = 0; /* Counts the number of characters printed */
-
-	va_list args;
-
-	va_start(args, format);
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-
-			switch (*format)
-			{
-				case 'c':
-					count += print_char(va_arg(args, int));
-					break;
-				case 's':
-					count += print_string(va_arg(args, char *));
-					break;
-				case '%':
-					putchar('%');
-					count++;
-					break;
-				default:
-					break;
-			}
-		}
-		else
-		{
-			count += print_char(*format);
-		}
-
-		format++;
-	}
-
-	va_end(args);
-
-	return (count);
+        return count;
 }
