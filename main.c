3a877e21199b51b755fe7403c44e5055d67eaae4
Ayanda Sibanda <ayanda.sibanda17@gmail.com>
2023-06-29 18:24:45 -0700

    Adding files

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
+int print_custom_format(void)
+{
+    printf("Custom format specifier %%r\n");
+    return 0;
+}
+
 int main(void)
 {
     int len;
@@ -38,7 +39,7 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
-    return (0);
+    print_custom_format();
+    return 0;
 }
+
